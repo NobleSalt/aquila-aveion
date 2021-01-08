fb9164fa0e266949a165ebd04c5b03cadd646a30
noblesalt <izuchukwue2@gmail.com>
2021-01-08 16:59:56 +0100

    initial

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="Home">
+    <h2 class="head">{{userInfo.name}}</h2>
+    <div class="img">
+      <img src="img/girl.png" alt="Girl with Orange" />
+    </div>
+    <div class="socials">
+      <a :href="userInfo.social_media.instagram">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a :href="userInfo.social_media.twitter">
+        <i class="fab fa-twitter-square"></i>
+      </a>
+      <a :href="userInfo.social_media.snapchat">
+        <i class="fab fa-snapchat"></i>
+      </a>
+      <a :href="'mailto:' + userInfo.social_media.email">
+        <i class="fas fa-envelope-square"></i>
+      </a>
+    </div>
+
+    <div class="links">
+      <a href="#" class="link">home</a>
+      <a href="#" class="link">about</a>
+      <a href="#" class="link im"><img src="img/model-hair.png" alt=""/></a>
+      <a href="#" class="link">gallery</a>
+      <a href="#" class="link">contact</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      userInfo: state => state.userInfo
+    })
+  },
+  created() {
+    this.$store.dispatch('GetUserInfo')
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/home.scss';
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,29 @@
+import axios from 'axios'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userInfo: {}
   },
   mutations: {
+    handleGetUserInfo(state, info) {
+      state.userInfo = info
+    }
   },
   actions: {
-  },
-  modules: {
+    GetUserInfo({ commit }) {
+      axios
+        .get('https://hirng-x2021.glitch.me/api/')
+        .then(res => {
+          const info = res.data
+          console.log(info)
+          commit('handleGetUserInfo', info)
+        })
+        .catch(e => {
+          console.log(new Error(e))
+          const info = undefined
+          commit('handleGetUserInfo', info)
+        })
+    }
   }
 })
